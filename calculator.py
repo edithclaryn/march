fe3a4ce7f50fc25191e571abb657b375f257c923
edithclaryn <edithnyamoita90@gmail.com>
2017-03-21 17:48:06 +0300

    calculator basic

---
+++ calculator.py
@@ -0,0 +1,11 @@
+""" You've finished eating at a restaurant, and received this bill:
+
+Cost of meal: $44.50
+Restaurant tax: 6.75%
+Tip: 15%
+You'll apply the tip to the overall cost of the meal (including tax)."""
+# Assign the variable meal the value 44.50 on line 3!
+meal = 44.50
+tax = 0.0675
+tip = 0.15
+print (meal + tax + tip)
