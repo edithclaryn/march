c91ba8e00d7a4cb6b00c32bb88ce21a793437fad
edithclaryn <edithnyamoita90@gmail.com>
2017-03-22 21:51:37 +0300

    string concatenation

---
+++ stringconcatenatetest.py
@@ -0,0 +1,15 @@
+"""Create a function that concatenates strings.
+Define a function called join_strings accepts an argument called words. It will be a list.
+Inside the function, create a variable called result and set it to "", an empty string.
+Iterate through the words list and append each word to result.
+Finally, return the result.
+Don't add spaces between the joined strings """
+
+n = ["Michael", "Lieberman"]
+
+def join_strings(words):
+    result = ""
+    for i in range(0,len(words)):
+        result = result + words[i]
+    return result
+print (join_strings(n))
