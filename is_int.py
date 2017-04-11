56203458e230d593b28042a0d0a0b6b06509c71a
edithclaryn <edithnyamoita90@gmail.com>
2017-04-11 11:22:52 +0300

    get integer

---
+++ is_int.py
@@ -0,0 +1,7 @@
+"""Define a function is_int that takes a number x as an input.
+Have it return True if the number is an integer and False otherwise"""
+def is_int(x):
+    if  x == int(x):
+        return True
+    else:
+        return False
